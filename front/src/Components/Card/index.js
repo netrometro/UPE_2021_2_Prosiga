700ccdeccccfdd75fd8944d0eb9be0ee7013b765
Rafael Souza <rafaeljosegomes25@hotmail.com>
2022-05-07 14:25:46 -0300

    definidas propriedades dos cards

--- front/src/Components/Card/index.js
+++ front/src/Components/Card/index.js
@@ -1,32 +1,37 @@
 import * as React from 'react';
 import Card from '@mui/material/Card';
-import CardActions from '@mui/material/CardActions';
 import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
-import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
+import { Button, CardActionArea, CardActions } from '@mui/material';
+import Upe from '../../Assets/upe-vector.png'
 
-export default function ImgMediaCard() {
+export default function MultiActionAreaCard(props) {
+  const {
+    titulo,
+    descricao,
+  } = props;
+
   return (
     <Card sx={{ maxWidth: 345 }}>
       <CardMedia
         component="img"
-        alt="green iguana"
         height="140"
-        image="/static/images/cards/contemplative-reptile.jpg"
+        image={Upe}
+        alt="green iguana"
       />
       <CardContent>
         <Typography gutterBottom variant="h5" component="div">
-          Lizard
+          {titulo}
         </Typography>
         <Typography variant="body2" color="text.secondary">
-          Lizards are a widespread group of squamate reptiles, with over 6,000
-          species, ranging across all continents except Antarctica
+          {descricao}
         </Typography>
       </CardContent>
       <CardActions>
-        <Button size="small">Share</Button>
-        <Button size="small">Learn More</Button>
+        <Button size="small" color="primary" variant='contained' fullWidth>
+          Acessar
+        </Button>
       </CardActions>
     </Card>
   );
