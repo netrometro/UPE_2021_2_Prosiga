62b0c8b1c7a4e84000a929ff10b83cd283df524e
Rafael Souza <rafaeljosegomes25@hotmail.com>
2022-05-07 15:18:55 -0300

    criada lista de cards para exibir cursos

--- front/src/App.js
+++ front/src/App.js
@@ -1,7 +1,9 @@
 import logo from './logo.svg';
 import './App.css';
 import Header from './Components/Header/index';
+import Card from './Components/Card/index';
 import AccountCircleIcon from '@mui/icons-material/AccountCircle';
+import { Grid } from '@mui/material';
 
 function App() {
 
@@ -18,7 +20,7 @@ function App() {
       case "opção3":
         alert("Entrar na aba Sobre Nós")
         break
-      
+
       // opções da direita
       case "Perfil":
         alert("Acessar a página de perfil")
@@ -26,6 +28,8 @@ function App() {
       case "Meus Cursos":
         alert("Ver seus cursos")
         break
+      default:
+        alert(page)
     }
   }
 
@@ -36,13 +40,48 @@ function App() {
 
   return (
     <div className="App">
-      <Header 
+      <Header
         logo="Teste"
         pages={["opção1", "opção2", "opção3", "opção4", "opção5"]}
         settings={['Perfil', 'Meus Cursos', 'Descobrir Cursos', 'Logout']}
         funcaoCustomizada={alertTeste}
         iconeEscolhido={renderIcon}
       />
+      <h1>Todos os Cursos</h1>
+      <hr></hr>
+      <Grid container spacing={1} columns={12}>
+        <Grid item xs={3}>
+          <Card 
+            titulo="Engenharia de Software"
+            acessarCurso={alertTeste}
+          />
+        </Grid>
+        <Grid item xs={3}>
+          <Card 
+            titulo="História"
+          />
+        </Grid>
+        <Grid item xs={3}>
+          <Card 
+            titulo="Psicologia"
+          />
+        </Grid>
+        <Grid item xs={3}>
+          <Card 
+            titulo="Medicina"
+          />
+        </Grid>
+        <Grid item xs={3}>
+          <Card 
+            titulo="Medicina"
+          />
+        </Grid>
+        <Grid item xs={3}>
+          <Card 
+            titulo="Medicina"
+          />
+        </Grid>
+      </Grid>
     </div>
   );
 }
