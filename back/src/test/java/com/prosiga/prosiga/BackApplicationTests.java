1a5064ea907862375009ff8c3fce8eded9ce6297
rafael-jose <rafaeljosegomes25@hotmail.com>
2022-05-14 16:09:56 -0300

    spring start

---
+++ back/src/test/java/com/prosiga/prosiga/BackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prosiga.prosiga;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
