b566bebbb077ca05ebec78e776f7e792c39a2939
Marx <marx.santos@upe.br>
2022-05-15 22:03:21 -0300

    Rota para tela Login

--- front/src/routes.js
+++ front/src/routes.js
@@ -2,7 +2,7 @@ import React, { useContext } from 'react';
 import { Routes, Route } from 'react-router-dom';
 import Cursos from './Screens/Cursos/index';
 import Home from './Screens/Home/homepage';
-
+import Login from './Screens/Login/index'
 
 export default function CustomRoutes() {
     return (
@@ -14,6 +14,10 @@ export default function CustomRoutes() {
             <Route 
                 path="/cursos" 
                 element={<Cursos />}
+            />
+            <Route 
+                path="/login" 
+                element={<Login />}
             />
         </Routes>
     );
