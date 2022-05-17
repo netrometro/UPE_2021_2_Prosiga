436632cde5f68ae2468703c037c40ba6dede3175
rafael-jose <rafaeljosegomes25@hotmail.com>
2022-05-17 14:22:22 -0300

    controlado estado do header

--- front/src/Screens/Cursos/index.js
+++ front/src/Screens/Cursos/index.js
@@ -6,8 +6,47 @@ import AccountCircleIcon from '@mui/icons-material/AccountCircle';
 import { Grid } from '@mui/material';
 
 function Cursos() {
+
+  // função customizada de exemplo
+  const alertTeste = (page) => {
+    switch (page) {
+      // opções do menu da esquerda
+      case "opção1":
+        alert("Voltar para a Home")
+        break
+      case "opção2":
+        alert("Entrar na aba de contato")
+        break
+      case "opção3":
+        alert("Entrar na aba Sobre Nós")
+        break
+
+      // opções da direita
+      case "Perfil":
+        alert("Acessar a página de perfil")
+        break
+      case "Meus Cursos":
+        alert("Ver seus cursos")
+        break
+      default:
+        alert(page)
+    }
+  }
+
+  // função de ícone da sua escolha
+  const renderIcon = () => {
+    return <AccountCircleIcon />
+  }
+
   return (
     <div className="App">
+      <Header
+        logo="Teste"
+        pages={["opção1", "opção2", "opção3", "opção4", "opção5"]}
+        settings={['Perfil', 'Meus Cursos', 'Descobrir Cursos', 'Logout']}
+        funcaoCustomizada={alertTeste}
+        iconeEscolhido={renderIcon}
+      />
       <h1>Todos os Cursos</h1>
       <hr></hr>
     </div>
