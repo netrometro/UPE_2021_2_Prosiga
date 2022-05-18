6c09613c57c22d385c8442e333b01564631ce9ec
rafael-jose <rafaeljosegomes25@hotmail.com>
2022-05-18 14:38:28 -0300

    criada entidade inicial de curso

---
+++ back/src/main/java/com/prosiga/prosiga/domain/Curso.java
@@ -0,0 +1,19 @@
+package com.example.prosiga.prosiga;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Curso {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String titulo;
+    private String descricao;
+}
