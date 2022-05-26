3688c68438fc25489080cb69dc1059207281c777
rafael-jose <rafaeljosegomes25@hotmail.com>
2022-05-25 21:00:15 -0300

    adicionado decorador de servlet

--- back/src/main/java/com/prosiga/prosiga/BackApplication.java
+++ back/src/main/java/com/prosiga/prosiga/BackApplication.java
@@ -2,7 +2,9 @@ package com.prosiga.prosiga;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 
+@ServletComponentScan
 @SpringBootApplication
 public class BackApplication {
 
